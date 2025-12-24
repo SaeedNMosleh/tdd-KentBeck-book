b3d0ef47ad2bf552c3431d4d257633c53782d2cc
Saeed <saeed.najafi.pl@gmail.com>
2025-12-24 23:00:08 +0100

    add franc class

--- src/money.py
+++ src/money.py
@@ -1,9 +1,23 @@
-class Dollar:
+class Moeny:
     def __init__(self,amount) -> None:
-        self.amount = amount
+        self._amount = amount
+
+class Dollar(Moeny): 
+    def __init__(self,amount) -> None:
+        super().__init__(amount)        
     def times(self,multiplier):
         #self.amount *= multiplier
-        return Dollar(self.amount*multiplier)
+        return Dollar(self._amount*multiplier)
     # Sample equal method -> it needs to be replaced with __eq__
     def equal(self, other):
-        return self.amount == other.amount if isinstance(other, Dollar) else False
+        return self._amount == other._amount if isinstance(other, Dollar) else False
+
+class Franc:
+    def __init__(self,amount) -> None:
+        self.__amount = amount
+    def times(self,multiplier):
+        #self.amount *= multiplier
+        return Franc(self.__amount*multiplier)
+    # Sample equal method -> it needs to be replaced with __eq__
+    def equal(self, other):
+        return self.__amount == other.__amount if isinstance(other, Franc) else False
--- tests/test_money.py
+++ tests/test_money.py
@@ -1,11 +1,14 @@
-from src.money import Dollar
-def test_multiplication():
+from src.money import Dollar, Franc
+def testMultiplication():
     five = Dollar(5)
-    product = five.times(2);
-    assert product.amount == 10 
-    product = five.times(3)
-    assert product.amount == 15
+    assert five.times(2).equal(Dollar(10))
+    assert five.times(3).equal(Dollar(15))
 
 def testEquality():
     assert Dollar(5).equal(Dollar(5))
     assert not Dollar(5).equal(Dollar(6))
+
+def testFrancMultiplication():
+    five = Franc(5)
+    assert five.times(2).equal(Franc(10))
+    assert five.times(3).equal(Franc(15))
